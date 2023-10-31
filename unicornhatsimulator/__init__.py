f56da624967aefcca1b5db197731587dc51be625
Matthew Brash <matthew.brash@ocsb.ca>
2023-10-30 20:52:11 -0400

    Re-enabled pygame.init()

--- unicornhatsimulator/__init__.py
+++ unicornhatsimulator/__init__.py
@@ -60,7 +60,7 @@ class UnicornHatSim(object):
         self.window_height = height * self.pixel_size
 
         # Init pygame and off we go
-        # pygame.init()   
+        pygame.init()   
         pygame.display.set_caption("Unicorn HAT simulator")
         self.screen = pygame.display.set_mode(
             [self.window_width, self.window_height])
